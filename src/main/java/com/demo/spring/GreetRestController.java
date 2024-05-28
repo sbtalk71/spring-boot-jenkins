85e7c0b26af9acdba11037741d03c3682752cdca
sbtalk71 <sbtalk@yahoo.com>
2024-05-28 11:40:02 +0530

    project commit2

---
+++ src/main/java/com/demo/spring/GreetRestController.java
@@ -0,0 +1,13 @@
+package com.demo.spring;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetRestController {
+
+	@GetMapping("/greet")
+	public String getGreet() {
+		return "Greetings from Spring REST";
+	}
+}
--- src/main/java/com/demo/spring/SpringBootJenkinsApplication.java
+++ src/main/java/com/demo/spring/SpringBootJenkinsApplication.java
@@ -10,4 +10,5 @@ public class SpringBootJenkinsApplication {
 		SpringApplication.run(SpringBootJenkinsApplication.class, args);
 	}
 
+	
 }
